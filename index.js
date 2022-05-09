a20025b0f226a57ff9fb07fdf1910132bda03acf
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-05-08 18:46:06 -0700

    sumItems function created

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let total = 0;
+  // RECURSIVE CASE: Sum all the numbers in the array
+  for (let ele of array) {
+    if (Array.isArray(ele)) {
+      total += sumItems(ele);
+    } else {
+    total += ele;
+  }}
+  return total;
 }
 
 module.exports = sumItems;
